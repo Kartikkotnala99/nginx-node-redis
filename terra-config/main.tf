acbf9c3e67f74bdbabf692f4da583ae939a0d8f3
kartikkotnala99 <kartikworkspace5@gmail.com>
2026-04-30 15:05:06 +0530

    main.tf

--- terra-config/main.tf
+++ terra-config/main.tf
@@ -2,6 +2,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+
 resource "aws_security_group" "web_sg" {
   name        = "nginx-web-sg"
   description = "Allow HTTP inbound traffic"
@@ -29,29 +52,13 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
 
-data "aws_ami" "ubuntu" {
-    most_recent = true
-    owners = ["amazon"]
-  
-      filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
-      }
-
-      filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-      }
-    }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  monitoring             = true
 
   user_data = <<-EOF
               #!/bin/bash
@@ -69,6 +76,37 @@ resource "aws_instance" "web" {
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.web.public_ip
+
+
+resource "aws_sns_topic" "alerts" {
+  name = "nginx-docker-alerts"
 }
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = "kartikkotnala05@gmail.com"
+}
+
+
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "nginx-docker-cpu-high"
+  alarm_description   = "CPU above 50% for 2 consecutive minutes"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 50
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    InstanceId = aws_instance.web.id
+  }
+
+  alarm_actions = [aws_sns_topic.alerts.arn]
+  ok_actions    = [aws_sns_topic.alerts.arn]
+}
+
