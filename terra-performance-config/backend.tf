ff388d9f067710ba404061c8ad83043ae812062d
kartikkotnala99 <kartikworkspace5@gmail.com>
2026-04-30 14:38:04 +0530

    html

--- terra-performance-config/backend.tf
+++ terra-performance-config/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "pravesh-ebs-terra-performance-bucket"
+    bucket         = "kartik-ebs-terra-performance-bucket"
     key            = "eb-demo/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
